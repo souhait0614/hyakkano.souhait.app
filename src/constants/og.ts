17ec7553c363d789192f24245bda8dfad5098cb7
souhait <me@souhait.me>
2025-10-05 00:00:19 +0900

    og修正

--- src/constants/og.ts
+++ src/constants/og.ts
@@ -2,14 +2,13 @@
 // SPDX-License-Identifier: MIT
 
 import type { OpenGraph } from '@/types/OpenGraph';
-import { SITE_BASE_URL } from '@/constants/site';
+import { SITE_BASE_URL, SITE_NAME } from '@/constants/site';
 
 export const OG_DEFAULT = {
   ogType: 'website',
   ogCardType: 'summary',
   ogImageUrl: new URL('/open-graph.png', SITE_BASE_URL).href,
-  // TODO: 適切な alt を設定する
-  ogImageAlt: '',
+  ogImageAlt: `${SITE_NAME}のロゴ`,
   ogImageType: 'image/png',
   ogImageWidth: 384,
   ogImageHeight: 384,
