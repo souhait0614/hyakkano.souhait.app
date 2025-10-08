a8d96b780f94c9b6add9d92944bc8939178951e5
souhait <me@souhait.me>
2025-10-08 23:26:41 +0900

    サイト全体の情報を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import type { PageProps } from 'waku/router';
 
 import type { Metadata } from '@/types/Metadata';
+import LinkEmail from '@/components/LinkEmail';
 import LinkText from '@/components/LinkText';
-import { SITE_NAME } from '@/constants/site';
+import { SITE_CREATOR_SITE_URL, SITE_NAME, SITE_REPO_URL } from '@/constants/site';
 import PageHead from '@/features/PageHead';
 
 import { metadata as pageBirthdayMetadata } from './birthday';
@@ -16,12 +17,21 @@ export default function Page(pageProps: PageProps<'/'>) {
   return (
     <>
       <PageHead metadata={metadata} pageProps={pageProps} />
-      <div className='page-container h-full justify-center'>
-        <h1 className='sr-only'>{metadata.title}</h1>
-        <article className='card'>
-          <h2><LinkText to='/birthday'>{pageBirthdayMetadata.title}</LinkText></h2>
-          <p>{pageBirthdayMetadata.description}</p>
-        </article>
+      <div className='page-container h-full'>
+        <h1 className='page-title'>{metadata.title}</h1>
+        <div className='flex grow flex-col justify-center'>
+          <article className='card'>
+            <h2><LinkText to='/birthday'>{pageBirthdayMetadata.title}</LinkText></h2>
+            <p>{pageBirthdayMetadata.description}</p>
+          </article>
+        </div>
+        <section className='card'>
+          <h2 className='text-lg'>このサイトについて</h2>
+          <p>管理人が趣味で制作した100カノに関連した細々としたアプリを置く非公式のサイトです。</p>
+          <p><strong>公式とは一切関係ありません。</strong></p>
+          <p>サイトに関するお問い合わせは、メール（<LinkEmail user='me' domain='souhait.me' />）または<LinkText external href={SITE_CREATOR_SITE_URL}>管理人の各種SNSアカウント</LinkText>までお願いします。</p>
+          <p>このサイトのソースコードは<LinkText external href={SITE_REPO_URL}>GitHub</LinkText>で公開されています。</p>
+        </section>
       </div>
     </>
   );
