d61a56337ec325c9fa52f67e0552279affe68ec4
souhait <me@souhait.me>
2026-01-02 14:03:12 +0900

    コピーライトを2025から2026に更新

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -2,7 +2,7 @@ export const SITE_BASE_URL = new URL(import.meta.env.VITE_SITE_URL);
 
 export const SITE_NAME = '100カノ関連アプリ置き場';
 
-export const SITE_COPYRIGHT = '©2025 すえ';
+export const SITE_COPYRIGHT = '©2026 すえ';
 
 export const SITE_REPO_URL = 'https://github.com/souhait0614/hyakkano.souhait.app';
 export const SITE_CREATOR_SITE_URL = 'https://souhait.me';
