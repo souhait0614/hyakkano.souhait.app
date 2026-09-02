68195e06c392417e7a42286f62a08c1ed7f361c7
souhait <me@souhait.me>
2026-09-03 00:54:13 +0900

    恋太郎ファミリーの日常 第8話に登場したキャラクターに対応

--- data/seiyuus/anime.ts
+++ data/seiyuus/anime.ts
@@ -145,10 +145,9 @@ const seiyuusAnimeArray = [
   ['seiyuu_anime_misaki_nako', {
     name: { kanji: ['岬', 'なこ'], hiragana: ['みさき', 'なこ'] },
   }],
-  // TODO: 羽香里の父の名前が判明したら追加
-  // ['seiyuu_anime_ishida_akira', {
-  //   name: { kanji: ['石田', '彰'], hiragana: ['いしだ', 'あきら'] },
-  // }],
+  ['seiyuu_anime_ishida_akira', {
+    name: { kanji: ['石田', '彰'], hiragana: ['いしだ', 'あきら'] },
+  }],
   ['seiyuu_anime_igoma_yurie', {
     name: { kanji: ['伊駒', 'ゆりえ'], hiragana: ['いごま', 'ゆりえ'] },
   }],
